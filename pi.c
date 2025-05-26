5bd3b923c8290d72f78522c3c51488f212eae03d
unsurp4ssed <postbox6310@gmail.com>
2025-05-26 15:22:16 +0300

    final c

--- pi.c
+++ pi.c
@@ -3,9 +3,12 @@
 #include <string.h>
 #include <math.h>
 
+//increase stack and use split stacks for recursions deeper than couple hundred k
+//gcc -lm -Wl,-z,stack-size=4194304 -fsplit-stack pi.c -o pi.c.out -lm
+
 double calcPI(long long int n) {
 	if (n == 1) return 4;
-	return 4*pow(-1,n+1)*(1/(double)(2*n-1))+calcPI(n-1);
+	return 4*pow(-1, n+1)*(1/(double)(2*n-1))+calcPI(n-1);
 }
 
 int main(int argc, char *argv[]) {
@@ -17,7 +20,9 @@ int main(int argc, char *argv[]) {
     if (strcmp(argv[1], "--help") == 0) {goto exit;}
 
     double pi = 0;
-    long long int n = atoll(argv[1]);
+    long long int n;
+    n = atoll(argv[1]);
+    if (!n) {goto exit;}
     
 	printf("calcuating pi...\n");
 	pi = calcPI(n);
