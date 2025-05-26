706a1e175ef9ca0ca0d20cc5a2625db943ea9ccb
unsurp4ssed <postbox6310@gmail.com>
2025-05-26 13:56:03 +0300

    init

---
+++ pi.py
@@ -0,0 +1,26 @@
+import sys
+sys.setrecursionlimit(1000000)
+
+
+def calcPI(n):
+	if n == 1:
+		return 4
+	return 4 * (-1)**(n+1) * (1/(2*n-1)) + calcPI(n-1)
+
+def main():
+	try:
+		n = sys.argv[1]
+	except IndexError:
+		n = ''
+	help = ['h', '-h', '-help', '--help', '']
+	if (n in help) or (int(n)<=0): 
+		print('This program calculates PI in [n] iterations.\nn has to be no more than 1.000.000\nUsage: py pi.py [n]');
+		sys.exit(0);	
+
+	print('calcuating pi...');
+	pi = calcPI(int(n));
+	print('...done.')
+	print('PI is approx.:', pi)
+
+if __name__ == "__main__": 
+	main()
